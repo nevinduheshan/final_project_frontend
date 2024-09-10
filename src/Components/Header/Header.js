bf92934af43a8fe3ee6a879f910b4c2dcacdba0f
nevinduheshan <nevinduheshan4@gmail.com>
2024-09-10 23:14:54 +0530

    create prediction page visibility

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -47,9 +47,12 @@ function Header() {
                         <li>
                             <Link to="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</Link>
                         </li>
-                        <li>
-                            <Link to="/prediction" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Prediction</Link>
-                        </li>
+                         {/* Conditionally render the Prediction link if the user is logged in */}
+                         {token && (
+                            <li>
+                                <Link to="/prediction" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Prediction</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact Us</Link>
                         </li>
