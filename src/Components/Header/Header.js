776cc1e600427f0df3524c82c581736177fab93b
nevinduheshan <nevinduheshan4@gmail.com>
2024-09-13 12:25:33 +0530

    chage colors

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -36,7 +36,7 @@ function Header() {
                         <Link to="/login">
                             <button 
                                 type="button" 
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Login
                             </button>
                         </Link>
@@ -47,19 +47,19 @@ function Header() {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <Link to="/" className={`block px-3 rounded ${isActive('/') ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>Home</Link>
+                            <Link to="/" className={`block px-3 rounded ${isActive('/') ? 'text-white bg-blue-700 md:bg-transparent md:text-green-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>Home</Link>
                         </li>
                         <li>
-                            <Link to="/about" className={`block px-3 rounded ${isActive('/about') ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>About Us</Link>
+                            <Link to="/about" className={`block px-3 rounded ${isActive('/about') ? 'text-white bg-blue-700 md:bg-transparent md:text-green-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>About Us</Link>
                         </li>
                          {/* Conditionally render the Prediction link if the user is logged in */}
                          {token && (
                             <li>
-                                <Link to="/prediction" className={`block px-3 rounded ${isActive('/prediction') ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>Prediction</Link>
+                                <Link to="/prediction" className={`block px-3 rounded ${isActive('/prediction') ? 'text-white bg-green-700 md:bg-transparent md:text-green-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>Prediction</Link>
                             </li>
                         )}
                         <li>
-                            <Link to="/contact" className={`block px-3 rounded ${isActive('/contact') ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>Contact Us</Link>
+                            <Link to="/contact" className={`block px-3 rounded ${isActive('/contact') ? 'text-white bg-green-700 md:bg-transparent md:text-green-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-green-700 md:dark:hover:bg-transparent dark:border-gray-700'}`}>Contact Us</Link>
                         </li>
                     </ul>
                 </div>
--- src/Components/Pages/Register/Register.js
+++ src/Components/Pages/Register/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../assets/Logo.png'
 
 const Register = () => {
   const [username, setUsername] = useState('');
@@ -42,55 +43,55 @@ const Register = () => {
     <div>
 
 
-    <section class="bg-gray-50 dark:bg-gray-900">
-  <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-20 lg:py-16 lg:grid-cols-12">
-      <div class="w-full place-self-center lg:col-span-6">
-          <div class="p-6 mx-auto bg-white rounded-lg shadow dark:bg-gray-800 sm:max-w-xl sm:p-8">
-              <Link href="#" class="inline-flex items-center mb-4 text-xl font-semibold text-gray-900 dark:text-white">
-                  <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-                  Flowbite    
+      <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-20 lg:py-16 lg:grid-cols-12">
+          <div class="w-full place-self-center lg:col-span-6">
+            <div class="p-6 mx-auto bg-white rounded-lg shadow dark:bg-gray-800 sm:max-w-xl sm:p-8">
+              <Link to="/" className="inline-flex items-center text-2xl font-semibold text-gray-900">
+                <img src={logo} className="h-14 mr-4" alt="Site Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Tea sales</span>
               </Link>
               <h1 class="mb-2 text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
-                  Register Form
+                Register Form
               </h1>
               <p class="text-sm font-light text-gray-500 dark:text-gray-300">
-                  Start your website in seconds. Have an account? <Link to="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In here</Link>.
+                Start your website in seconds. Have an account? <Link to="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In here</Link>.
               </p>
               <form class="mt-4 space-y-6 sm:mt-6" onSubmit={handleSubmit}>
-                  <div>
-                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                      <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Name" required/>
-                  </div>
-                  <div>
-                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Username" required/>
-                  </div>
-                  <div>
-                      <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                      <input type="phone" value={phone} onChange={(e) => setPhone(e.target.value)} name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="0712345678" required/>
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Password" required/>
-                  </div>
+                <div>
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                  <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Name" required />
+                </div>
+                <div>
+                  <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                  <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Username" required />
+                </div>
+                <div>
+                  <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+                  <input type="phone" value={phone} onChange={(e) => setPhone(e.target.value)} name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="0712345678" required />
+                </div>
+                <div>
+                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Password" required />
+                </div>
 
-                  {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-                  {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
-                  
-                  <div class="flex items-center">
-                      <div class="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
-                      <div class="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
-                  </div>
-                 
-                  <button type="submit" class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
+                {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+
+                <div class="flex items-center">
+                  <div class="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
+                  <div class="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+
+                <button type="submit" class="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
               </form>
+            </div>
           </div>
-      </div>
-      <div class="mr-auto place-self-center lg:col-span-6">
-          <img class="hidden mx-auto lg:flex" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/authentication/illustration.svg" alt="illustration"/>  
-      </div>             
-  </div>
-    </section>
+          <div class="mr-auto place-self-center lg:col-span-6">
+            <img class="hidden mx-auto lg:flex" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/authentication/illustration.svg" alt="illustration" />
+          </div>
+        </div>
+      </section>
 
 
       {/* <h2>Registerfdsfsdf</h2>
